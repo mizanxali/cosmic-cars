30b8c891014f709247133492f40ecd3edc876918
Mizanali Panjwani <mizanalip786@gmail.com>
2022-04-11 06:22:09 +0530

    Create canvas, add lighting and 3D car models

--- src/App.js
+++ src/App.js
@@ -1,5 +1,76 @@
+import { OrbitControls } from '@react-three/drei';
+import { Canvas, useLoader } from '@react-three/fiber';
+import { Suspense, useState } from 'react';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import './App.css';
+import CARS from './cars';
 
 export default function App() {
-  return <div className="App">hello world</div>;
+  const [currentCar, setCurrentCar] = useState(0);
+
+  function goLeft() {
+    if (currentCar !== 0) {
+      const newCar = currentCar - 1;
+      console.log(newCar);
+      setCurrentCar(newCar);
+    }
+  }
+
+  function goRigt() {
+    if (currentCar !== CARS.length - 1) {
+      const newCar = currentCar + 1;
+      console.log(newCar);
+      setCurrentCar(newCar);
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="left">
+        <Canvas>
+          <axesHelper scale={100} />
+          <Lights />
+          <Suspense fallback={null}>
+            <Model currentCar={currentCar} />
+            <OrbitControls />
+            {/* <Environment preset="sunset" background /> */}
+          </Suspense>
+        </Canvas>
+      </div>
+      <div className="right">
+        <span onClick={goLeft}>LEFT</span>
+        <span onClick={goRigt}>RIGHT</span>
+        <h1>{CARS[currentCar].name}</h1>
+      </div>
+    </div>
+  );
 }
+
+const Lights = () => {
+  return (
+    <>
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[500, 0, 0]} intensity={1} />
+      <directionalLight position={[-500, 0, 0]} intensity={1} />
+      <directionalLight position={[0, 500, 0]} intensity={1} />
+      <directionalLight position={[0, 0, 500]} intensity={1} />
+      <directionalLight position={[0, 0, -500]} intensity={1} />
+    </>
+  );
+};
+
+const Model = ({ currentCar }) => {
+  const gltf = useLoader(GLTFLoader, `./${CARS[currentCar].key}/scene.gltf`);
+
+  return (
+    <>
+      <primitive
+        object={gltf.scene}
+        scale={CARS[currentCar].scale}
+        // position={[0, 230, -500]}
+        // scale={1}
+        position={[0, 0, 0]}
+      />
+    </>
+  );
+};
---
+++ src/cars.js
@@ -0,0 +1,114 @@
+const CARS = [
+  {
+    name: 'Bentley Continental GT',
+    key: '2011_bentley_continental_gt_supersports',
+    scale: 0.01
+  },
+  {
+    name: 'Kia Sportage',
+    key: '2017_kia_sportage',
+    scale: 0.02
+  },
+  {
+    name: 'Alfa Romeo 8C',
+    key: 'alfa-romeo_8c',
+    scale: 0.012
+  },
+  {
+    name: 'Aston Martin V12 Vantage',
+    key: 'aston-martin_v12_vantage',
+    scale: 0.012
+  },
+  {
+    name: 'Audi RS5',
+    key: 'audi_rs5',
+    scale: 1.5
+  },
+  {
+    name: 'BMW i8',
+    key: 'bmw_i8',
+    scale: 0.01
+  },
+  {
+    name: 'Bugatti Chiron',
+    key: 'bugatti_chiron',
+    scale: 0.012
+  },
+  {
+    name: 'Ferrari 458 Italia',
+    key: 'ferrari_458_italia',
+    scale: 0.012
+  },
+  {
+    name: 'Ford Mustang Shelby GT350R',
+    key: 'ford_mustang_gt-350r',
+    scale: 0.01
+  },
+  {
+    name: 'Jaguar C-X75',
+    key: 'jaguar_c-x75',
+    scale: 0.4
+  },
+  {
+    name: 'Koenigsegg One:1',
+    key: 'koenigsegg_one1',
+    scale: 0.5
+  },
+  {
+    name: 'Lamborghini Aventador J',
+    key: 'lamborghini_aventador_j',
+    scale: 0.0014
+  },
+  {
+    name: 'Mazda 3',
+    key: 'mazda_3',
+    scale: 0.0038
+  },
+  {
+    name: 'McLaren F1',
+    key: 'mclaren_f1',
+    scale: 0.0035
+  },
+  {
+    name: 'Mercedes-Benz SLS AMG',
+    key: 'mercedes-benz_sls_amg',
+    scale: 0.0017
+  },
+  {
+    name: 'Mistubishi Lancer Evolution X',
+    key: 'mitsubishi_lancer_evolution_x',
+    scale: 0.0032
+  },
+  {
+    name: 'Nissan GT-R',
+    key: 'nissan_gt-r',
+    scale: 0.0032
+  },
+  {
+    name: 'Peugeot 3008',
+    key: 'peugeot_3008',
+    scale: 0.0032
+  },
+  {
+    name: 'Peugeot Onyx',
+    key: 'peugeot_onyx',
+    scale: 0.41
+  },
+  {
+    name: 'Range Rover Evoque',
+    key: 'range_rover_evoque',
+    scale: 0.72
+  },
+  {
+    name: 'Tesla Model 3',
+    key: 'tesla_model_3',
+    scale: 0.0022
+  },
+  {
+    name: 'Tesla Model S',
+    key: 'tesla_model_s',
+    scale: 0.6
+  }
+];
+
+export default CARS;
